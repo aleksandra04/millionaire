03b32d02ef7af459abc582d17d4782e88c29d78e
Aleksandra <aleksandra@macbook2.local>
2020-10-04 23:18:52 +0300

    Updates

---
+++ precache-manifest.90f4d47c057b8e76e78ea615eefd452e.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c546be35e5d79db03632d114ddd85e63",
+    "url": "/millionaire/index.html"
+  },
+  {
+    "revision": "57e7bb4fd95aa94438cb",
+    "url": "/millionaire/static/css/main.da7696b2.chunk.css"
+  },
+  {
+    "revision": "13ff2c1a9f3e619378bb",
+    "url": "/millionaire/static/js/2.bf80a038.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/millionaire/static/js/2.bf80a038.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57e7bb4fd95aa94438cb",
+    "url": "/millionaire/static/js/main.58035c76.chunk.js"
+  },
+  {
+    "revision": "078e70291101148e5f5b",
+    "url": "/millionaire/static/js/runtime-main.70d563fc.js"
+  },
+  {
+    "revision": "512260346fddb0a0b605c14dd0e93017",
+    "url": "/millionaire/static/media/Close.51226034.svg"
+  },
+  {
+    "revision": "99b0a48b5f986c0493881ba28bc57346",
+    "url": "/millionaire/static/media/RectAnswerDesk.99b0a48b.svg"
+  },
+  {
+    "revision": "21d2920a6d07cd2639c55c9ca016b026",
+    "url": "/millionaire/static/media/RectAnswerDeskGreen.21d2920a.svg"
+  },
+  {
+    "revision": "25e34b4cde51f6606c8510c2e2f7a85d",
+    "url": "/millionaire/static/media/RectAnswerDeskOrangr.25e34b4c.svg"
+  },
+  {
+    "revision": "45eee39a7565ccbdba1f093e0a8534f3",
+    "url": "/millionaire/static/media/RectAnswerDeskRed.45eee39a.svg"
+  },
+  {
+    "revision": "6e72b952a58282c194b4894fc4388d74",
+    "url": "/millionaire/static/media/Rectangle.6e72b952.svg"
+  },
+  {
+    "revision": "8e6609ddfe1d6eaccba35ab308116831",
+    "url": "/millionaire/static/media/RectangleCostDesktop.8e6609dd.svg"
+  },
+  {
+    "revision": "03b3a8470a48b4fde2e0f729fe1ff3ff",
+    "url": "/millionaire/static/media/RectangleCostMobOrange.03b3a847.svg"
+  },
+  {
+    "revision": "0a99b51406acb2d5597fcd8f54401390",
+    "url": "/millionaire/static/media/RectangleCostMobile.0a99b514.svg"
+  },
+  {
+    "revision": "345fbf3acab69ec32c590023c951b387",
+    "url": "/millionaire/static/media/RectangleCostOrangeDesktop.345fbf3a.svg"
+  },
+  {
+    "revision": "f7017fbebcdaa8813f291ba7e46bf0ba",
+    "url": "/millionaire/static/media/RectangleGreen.f7017fbe.svg"
+  },
+  {
+    "revision": "7001afac675f483b2ef470b441ef7bb8",
+    "url": "/millionaire/static/media/RectangleOrange.7001afac.svg"
+  },
+  {
+    "revision": "f5bacdedf1f629869466bfda9a609f30",
+    "url": "/millionaire/static/media/RectangleRed.f5bacded.svg"
+  },
+  {
+    "revision": "276b2ba6b79973c3215716dd829c1be1",
+    "url": "/millionaire/static/media/hand.276b2ba6.svg"
+  },
+  {
+    "revision": "ef43c1810bdd75a0797d319ade32bc2b",
+    "url": "/millionaire/static/media/side.ef43c181.svg"
+  }
+]);
